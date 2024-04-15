31b509d5d808c2e3a4d583ad98c9a3d148628627
Aleksander Andersen <lordaust@gmail.com>
2024-04-15 21:03:59 +0200

    Initial commit

---
+++ sanity/src/defaultDocumentNode.ts
@@ -0,0 +1,44 @@
+import { isDev, type SanityDocument } from 'sanity'
+import { Iframe } from 'sanity-plugin-iframe-pane'
+import type { DefaultDocumentNodeResolver } from 'sanity/structure'
+
+const defaultDocumentNode: DefaultDocumentNodeResolver = (
+	S,
+	{ schemaType },
+) => {
+	switch (schemaType) {
+		case 'page':
+		case 'blog.post':
+			return S.document().views([
+				S.view.form(),
+				S.view
+					.component(Iframe)
+					.options({
+						url: (doc: SanityPage) => {
+							const domain = isDev
+								? 'http://localhost:3000'
+								: 'https://next-sanity-starter-template.vercel.app'
+
+							const slug = doc?.metadata?.slug?.current
+							const path = slug === 'index' ? '' : slug
+							const directory = schemaType === 'blog.post' ? 'blog' : null
+
+							return [domain, directory, path].filter(Boolean).join('/')
+						},
+						reload: {
+							button: true,
+						},
+					})
+					.title('Preview'),
+			])
+
+		default:
+			return S.document().views([S.view.form()])
+	}
+}
+
+export default defaultDocumentNode
+
+type SanityPage = SanityDocument & {
+	metadata?: { slug?: { current: string } }
+}
